a14d7e1e52aa73fe561211b32288f858dd553c76
Ben Haley <benjamin.haley@gmail.com>
2011-09-29 03:02:57 +0000

    package management that attempts to download a dependency if it cannot be found locally

---
+++ scripts/package.R
@@ -0,0 +1,31 @@
+# Scripts should be able to check for packages and load them if they are not
+# found.  This script will do exactly that.
+# bmh Sept 2011
+
+# Usage 
+#   source("package.R")
+#   dependencies <- "RCurl"
+#   package$load(dependencies)
+
+package <- list()
+package$.__REPOS <- "http://cran.r-project.org"
+
+package$load <- function( dependencies ){
+	installed <- .packages( all=TRUE )
+	missing <- dependencies[ ! dependencies %in% installed ]
+	apply( 
+		as.matrix(missing), 
+		1, 
+		install.packages, 
+		repos=package$.__REPOS, 
+		dependencies=TRUE 
+	 )
+	apply( 
+		as.matrix(dependencies), 
+		1, 
+		library, 
+		character.only=TRUE 
+	 )
+}
+
+
