28072f4f1171c4ba9e4bf5def6ca0180841e998f
Benjamin Haley <benjamin.haley@gmail.com>
2015-09-08 20:29:10 -0500

    A little more cleaning

--- linear_quadratic_exponential_is_not_sigmodial.R
+++
@@ -1,8 +0,0 @@
-a <- 1
-B <- 20000000000000
-e <- -0.7
-dose <- seq(0, 4, 0.01)
-
-response <- (a * dose + B * dose^2) * exp(dose * e)
-library(ggplot2)
-ggplot(NULL, aes(dose, response)) + geom_path()
